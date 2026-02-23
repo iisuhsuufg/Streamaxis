6d17392ba0b9a8e38612407c9b3a10f98de185ff
iisuhsuufg <103226110+iisuhsuufg@users.noreply.github.com>
2026-02-23 15:20:51 -0500

    Update Program.cs

--- StreamAxis.Api/Program.cs
+++ StreamAxis.Api/Program.cs
@@ -62,10 +62,11 @@ app.UseAuthorization();
 app.MapControllers();
 app.MapRazorPages();
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
-}
+//using (var scope = app.Services.CreateScope())
+//{
+ //   var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+ //   db.Database.Migrate();
+//}
+
 
 app.Run();
